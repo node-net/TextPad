9a6509d598f4ad040b62efb110791b19d0813a3f
Lou Parslow <lou.parslow@gmail.com>
2015-09-15 22:30:05 -0600

    'all'

---
+++ TextPad/TextPadDocument.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextPad
+{
+    public class TextPadDocument : System.Collections.Generic.List<string>
+    {
+        private System.IO.StreamReader streamReader = null;
+        public void Open(System.IO.Stream stream)
+        {
+            Clear();
+            streamReader = new System.IO.StreamReader(stream);
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                Add(line);
+            }
+        }
+        private System.IO.StreamWriter streamWriter = null;
+        public void Save(System.IO.Stream stream)
+        {
+            streamWriter = new System.IO.StreamWriter(stream);
+            foreach (string line in this)
+            {
+                streamWriter.WriteLine(line);
+            }
+            streamWriter.Flush();
+        }
+    }
+}
